4d4faa29028a0faa4fe6308b21db778c98593c25
peregra <peregra@gmail.com>
2019-07-31 21:07:58 -0500

    Down to 2 compile errors

--- project.c
+++ project.c
@@ -27,9 +27,8 @@ Sarah Baker, Muhammad Ghazi, Brittany Thibodeaux
 #define NULL
 #endif
 
-/**
-The C data structure used to save arguments
-*/
+// The C data structure used to save arguments
+
 struct command_t{
 	char *name;
 	int argc;
@@ -38,20 +37,20 @@ struct command_t{
 
 // Function prototypes
 char *lookupPath(char **, char **);
-int parseCommand(char *, struct command_t);
+int parseCommand(char *cLine, struct command_t *cmd);
 int parsePath(char **);
 void printPrompt();
 void readCommand(char *);
 
+// Global variable
+int i;
 
 int main(){
 	// Shell initialization
-    char commandLine[LINE_LEN];
+    char *commandLine;
     struct command_t command;
     char *pathv = getenv("PATH");
-    int i;
     bool run = true;
-    // List any remaining variables
 
     // Get directory paths from PATH
     parsePath(&pathv); 
@@ -61,22 +60,20 @@ int main(){
 
         // Read command line and parse
         readCommand(commandLine);
-        parseCommand(commandLine, command);
+        parseCommand(commandLine, &command);
 
         // Get full pathname for the file
         command.name = lookupPath(command.argv, pathv);
         if(command.name == NULL) {
-            // Report error
             continue;
         }
 
         // Create child and execute command
- 
-	if((childPID = fork())==0) {
-		execv(command.name, command.argv);
-		printf("Unknown command\n");
-		exit(0);
-            }
+        if(fork()==0) {
+            execv(command.name, command.argv);
+            printf("Unknown command\n");
+            exit(0);
+        }
     }
 	
     // Shell termination
@@ -86,30 +83,28 @@ int main(){
 int parsePath(char *dirs[]){
 	// Read the PATH variable for the environment then build dirs[], an array of
 	// directories in PATH
-	
+
 	char *pathEnvVar;
 	char *thePath;
 	
-	for(int i = 0; i < MAX_ARGS; i++){
+	for(i = 0; i < MAX_ARGS; i++){
 		dirs[i] = NULL;
 	}
+
 	pathEnvVar = (char *) getenv("PATH");
 	thePath = (char *) malloc(strlen(pathEnvVar) + 1);
 	strcpy(thePath, pathEnvVar);
 	
-	/**Lop to parse thePath. Look for a ':' delimiter between
-	each path name*/
-	while(int i = 0; i < MAX_ARGS; i++) {
-		dirs[i] = strsep(thePath, ":");
-	} // TODO Verify this solution
+	// Loop to parse thePath. Look for a ':' delimiter between
+	// each path name
+	for (i = 0; i < MAX_ARGS; i++) {
+		dirs[i] = strsep(&thePath, ":");
+	}
 }
 
 
-/**
-Build the prompt string to have the machine name,
-current directory, or other desired information
-*/
-
+// Build the prompt string to have the machine name, current directory, 
+// or other desired information
 void printPrompt(){
 	char *promptString = "% ";
 	printf("%s", promptString);
@@ -118,10 +113,9 @@ void printPrompt(){
 
 void readCommand(char *buffer) {
     // Read entire command line into the buffer
-    fgets(commandLine, LINE_LEN, stdin);
+    fgets(buffer, LINE_LEN, stdin);
 }
 
-// TODO Verify that this function works in this shell as well
 int parseCommand(char *cLine, struct command_t *cmd) {
     int argc;
     char **clPtr;
@@ -129,27 +123,25 @@ int parseCommand(char *cLine, struct command_t *cmd) {
     // Initialize
     clPtr = &cLine;     // cLine is command line
     argc = 0;
-    cmd->argv[argc] = (char *)malloc(MAX_ARG_LEN);
+    cmd->argv[argc] = (char *)malloc(MAX_ARG_LENGTH);
 
     // Fill argv[] 
     while((cmd->argv[argc] = strsep(clPtr, WHITESPACE)) != NULL) {
-        cmd->argv[++argc] = (char *)malloc(MAX_ARG_LEN);
+        cmd->argv[++argc] = (char *)malloc(MAX_ARG_LENGTH);
     }
 
     // Set command name and argc
     cmd->argc = argc - 1;
     cmd->name = (char *)malloc(sizeof(cmd->argv[0]));
     strcpy(cmd->name, cmd->argv[0]);
-    return 1;
 
+    return 1;
 }
 
 
-/*
-This funciton searches the directories identified ;by the dir
-argument to see if argv[0] (the file name) appears there. Allocate a new string,
-place the full path name in it, then return the string
-*/
+// Search directories identified by dir argument to see if argv[0] (the
+// file name) appears there. Allocate a new string, place the full path name in
+// it, then return the string
 char *lookupPath(char **argv, char **dir){
 	
 	char *result;
@@ -159,15 +151,16 @@ char *lookupPath(char **argv, char **dir){
 	//check to see if file name is already an absolute path name
 	if(*argv[0] == '/'){
 		return argv[0];
-	}//look in PATH directories.
+	}
+    
 	//Use access() to see if the file is in a dir
 	for(i = 0; i < MAX_PATHS; i++){
-		if(dirs[i] == NULL)
+		if(dir[i] == NULL)
 			continue;
 		
-		strcpy(tempDir,dirs[i]);
-		strncat(tempDir,"/",1);
-		strncat(tempDir,argv[0],strlen(argv[0]));
+		strcpy(tempDir, dir[i]);
+		strcat(tempDir, "/");
+		strcat(tempDir, argv[0]);
 		
 		if(access(tempDir, F_OK) == 0) {
 			return tempDir;
@@ -177,8 +170,4 @@ char *lookupPath(char **argv, char **dir){
 	//FILE NAME NOT FOUND
 	fprintf(stderr, "%s: command not found\n", argv[0]);
 	return NULL;
-}
-
-
-
 }
